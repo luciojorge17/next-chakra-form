89c72b89fd9cd622791e2437a6e7ff1644c9bb22
Lucio Jorge <luciojorge17@gmail.com>
2021-10-23 16:11:32 -0300

    inicialização do projeto

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Criação de Loja</title>
+            </Head>
+
+            <h1>Hello Next</h1>
+
+        </div>
+    )
+}
